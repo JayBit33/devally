dc95c3d1fa37b2f5824cd1b81e9b1e4aa2f0857c
Jay Roseman <jaybit33@gmail.com>
2020-12-27 00:03:18 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="main-component">
+    <div class="header">
+      <img alt="Vue logo" src="./assets/logo.png" id="img_logo" />
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Browse Projects</li>
+          <li>Search Devs</li>
+        </ul>
+      </nav>
+    </div>
+    <Main />
+    <div class="footer">
+      <p>&copy; DevAlly 2021</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Main from "./components/Main.vue";
+
+export default {
+  name: "App",
+  components: {
+    Main,
+  },
+};
+</script>
+
+<style lang="scss">
+body,
+html {
+  margin: 0;
+  padding: 0;
+}
+#main-component {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 1rem 0 0 0;
+}
+
+.header {
+  width: 100%;
+  padding: 1rem;
+  margin: 0 2rem;
+  color: black;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  nav {
+    overflow: hidden;
+    padding-right: 4rem;
+    ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+      li {
+        display: inline;
+        float: right;
+        text-decoration: none;
+        padding: 1rem 2rem;
+      }
+    }
+  }
+}
+
+#img_logo {
+  width: 3.75rem;
+}
+
+.footer {
+  background-color: #39c99d;
+  color: white;
+  text-align: center;
+  padding: 3rem 0 1.5rem 0;
+  margin-top: 2rem;
+}
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,38 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    state: {
+        user: {
+            guid: 0,
+            accountCreated: 'May 20 2021 7:32:23am',
+            firstName: 'Jay',
+            lastName: 'Boseman',
+            email: 'bosemanrocks@gmail.com',
+            password: 'HHLKHLEION2002NS102746179',
+            profileImage: 'images/user_profile.jpg',
+            accountType: ['ENTREPRENEUR', 'DEVELOPER'],
+            skills: ['Java', 'Spring Boot', 'REST API\'s', 'Mobile App Development'],
+            rating: 10,
+            availableHireOptions: ['OWNERSHIP PERCENTAGE', 'HOURLY', 'QUOTE']
+        }
+    },
+
+    getters: {
+        fullName(state) {
+            return state.user.firstName + ' ' + state.user.lastName
+        }
+    },
+
+    mutations: {
+
+    },
+
+    actions: {
+
+    }
+})
+
+export default store; 
