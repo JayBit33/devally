8950dd041352791e10c387180d2098f36cdb0585
roseman <jroseman@cincomcpq.com>
2021-01-11 00:13:33 -0500

    updated accounts route name to dev-accounts

--- backend/index.js
+++ backend/index.js
@@ -6,15 +6,16 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-const accounts = [
-  { id: 2200, username: 'jem', firstname: 'Jem', lastname: 'Ever', accountType: ['Entrepreneur','Professional'], skills: ['SPAs', 'Ecommerce', 'Websites', 'Graphic Design'], hiringOptions: ['Shares'], rating: 5},
-  { id: 2190, username: 'jen38950', firstname: 'Jen', lastname: 'Smith', accountType: ['Professional'], skills: ['SPAs', 'Ecommerce', 'Websites', 'Graphic Design'], hiringOptions: ['Hourly','Shares'], rating: 3},
-  { id: 2180, username: 'betho89', firstname: 'Beth', lastname: 'Antoitte', accountType: ['Entrepreneur'], skills: ['Java', 'Spring Boot', 'Backend Developer'], hiringOptions: ['Hourly','Shares'], rating: 4},
-  { id: 2170, username: 'appmaker', firstname: 'Ben', lastname: 'Hill', accountType: ['Professional'], skills: ['UX/UI', 'Graphic Design'], hiringOptions: ['Hourly','Shares'], rating: 4},
+const dev_accounts = [
+  { id: 2200, username: 'jeb', firstname: 'Jeb', lastname: 'Eichs', accountType: ['Entrepreneur','Professional'], skills: ['SPAs', 'Ecommerce', 'Websites', 'Graphic Design'], hiringOptions: ['Shares'], rating: 5, profileImage: 'profile3.jpg'},
+  { id: 2190, username: 'jen38950', firstname: 'Jen', lastname: 'Smith', accountType: ['Professional'], skills: ['SPAs', 'Ecommerce', 'Websites', 'Graphic Design'], hiringOptions: ['Hourly','Shares'], rating: 3, profileImage: 'profile5.jpg'},
+  { id: 2180, username: 'betho89', firstname: 'Beth', lastname: 'Antoitte', accountType: ['Entrepreneur'], skills: ['Java', 'Spring Boot', 'Backend Developer'], hiringOptions: ['Hourly','Shares'], rating: 4, profileImage: 'profile2.jpg'},
+  { id: 2170, username: 'appmaker', firstname: 'Ben', lastname: 'Hill', accountType: ['Professional'], skills: ['UX/UI', 'Graphic Design'], hiringOptions: ['Hourly','Shares'], rating: 4, profileImage: 'profile4.jpg'},
+  { id: 2171, username: 'cindyloo', firstname: 'Cindy', lastname: 'Summer', accountType: ['Professional'], skills: ['UX/UI', 'Graphic Design'], hiringOptions: ['Hourly','Shares'], rating: 4, profileImage: 'user_profile.jpg'},
 ]
-app.get('/accounts', (req,res) => {
-  console.log('accounts called');
-  res.send(accounts);
+app.get('/dev-accounts', (req,res) => {
+  console.log('dev-accounts called');
+  res.send(dev_accounts);
 })
 
 app.listen(port, () => console.log('server listening...'));
