c463cff592d67db69a6e3761d81114509ddf62e2
roseman <jroseman@cincomcpq.com>
2021-02-20 16:38:24 -0500

    once again updated user and project table schemas. added connection, settings info to susers and is_public and is_featured to projects

---
+++ backend/migrations/20210220150434_users.js
@@ -0,0 +1,38 @@
+// (c) Waveybits Inc. <2021>
+// ALL RIGHTS RESERVED
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id').primary();
+    table.string('username');
+    table.string('email');
+    table.string('password');
+    table.string('firstname');
+    table.string('lastname');
+    table.integer('rating').nullable();
+    table.bigInteger('user_type_id').unsigned().references('id').inTable('user_types').onDelete('CASCADE') // foreign key links user_type by id
+    table.string('profile_image').nullable();
+    table.json('connections').nullable(); // must use JSON.stringify(arraydata) when setting value
+    table.json('notification_settings').nullable(); // must use JSON.stringify(arraydata) when setting value
+    table.json('subscription_settings').nullable(); // must use JSON.stringify(arraydata) when setting value
+    table.timestamp('user_since').defaultTo(knex.fn.now());
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('users');
+};
+
+
+// WHEN RUNNING LATEST MIGRATIONS AND RUNNING SEEDS ORDER MATTERS!!!!!!!! SINCE USERS HAS USER_TYPE_ID USER_TYPES TABLE MIGRATION MUST BE MADE FIRST!
+// create migrations command: knex migrate:make create-tablename
+// run migrations command: knex migrate:latest
+// create seed for table command: knex seed:make 01_tablename
+// run seed command: knex seed:run
+
+
+// update tables
+// migrate:rollback -> migrate:latest -> seed:run
+
+// run specific file
+// knex seed:run --specific=01_users.js
