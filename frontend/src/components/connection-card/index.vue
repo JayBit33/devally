8753c137eae9e41ac17f4fa054c40dba5e7542fa
roseman <jroseman@cincomcpq.com>
2021-02-25 22:14:02 -0500

    changes connection card styles in user-connections

--- frontend/src/components/connection-card/index.vue
+++ frontend/src/components/connection-card/index.vue
@@ -2,6 +2,7 @@
 <!-- ALL RIGHTS RESERVED -->
 <template>
   <div class="connection-card">
+    <font-awesome-icon :icon="['fas','trash-alt']" class="delete-icon"></font-awesome-icon>
     <img class="connection-card-avatar" :src="require('../../assets/' + connection.profile_image)" />
     <h4 class="connection-card-fullname">{{ fullName }}</h4>
     <h5 class="connection-card-accountType">{{ accountType }}</h5>
@@ -9,8 +10,7 @@
       <li v-for="(role,i) in roles" :key="i">{{role}}</li>
     </ul>
     <div class="actions">
-      <button class="btn">Message</button>
-      <font-awesome-icon :icon="['fas','trash-alt']" class="delete-icon"></font-awesome-icon>
+      <!-- <font-awesome-icon :icon="['fas','envelope']" class="msg-icon"></font-awesome-icon> -->
     </div>
   </div>
 </template>
--- frontend/src/pages/user-app/user-connections/index.vue
+++ frontend/src/pages/user-app/user-connections/index.vue
@@ -4,7 +4,7 @@
   <div class="user-connections">
     <font-awesome-icon :icon="['fas','user-friends']" class="icon"></font-awesome-icon>
     <h1 class="title">User Connections</h1>
-    <!-- <filter-search /> -->
+    <!-- <filter-search  style="position: relative; left: 615px; top: -1px;" /> -->
     <div class="connections">
       <connection-card v-for="connection in connections" :connection="connection" :key="connection.id" />
     </div>
