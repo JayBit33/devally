31f354f4a7112c5e1cc3ee853c29b1af01afd236
roseman <jroseman@cincomcpq.com>
2021-02-28 18:43:21 -0500

    added temporary nav in connections view

--- frontend/src/pages/user-app/user-connections/index.vue
+++ frontend/src/pages/user-app/user-connections/index.vue
@@ -2,6 +2,10 @@
 <!-- ALL RIGHTS RESERVED -->
 <template>
   <div class="user-connections">
+    <ul class="nav">
+      <router-link to="/" class="link" ><li>Site Home</li></router-link>
+      <router-link to="/" class="link" ><li @click="$emit('optionSelected', 'signout')">Sign Out</li></router-link>
+    </ul>
     <font-awesome-icon :icon="['fas','user-friends']" class="icon"></font-awesome-icon>
     <h1 class="title">User Connections</h1>
     <!-- <filter-search  style="position: relative; left: 615px; top: -1px;" /> -->
