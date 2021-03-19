fbe2a18f3fa7a2ee3b96c8463e8bcd120ddfc2e9
roseman <jroseman@cincomcpq.com>
2021-03-18 20:32:40 -0400

    pagination mixin

---
+++ frontend/src/mixins/paginationMixin.js
@@ -0,0 +1,27 @@
+
+// (c) Waveybits Inc. <2021>
+// ALL RIGHTS RESERVED
+
+export default {
+  data() {
+    return {
+      pageSize: 5,
+      currentPageIdx: 1,
+      startIdx: 0,
+      endIdx: 5,
+    }
+  },
+  methods: {
+    updateDisplayedUsers(page) {
+      if (page === 1) {
+        this.startIdx = 0;
+        this.currentPageIdx = 1
+      } else {
+        this.startIdx = (page * this.pageSize) - this.pageSize;
+        this.currentPageIdx = page
+      }
+      this.endIdx = this.pageSize * page;
+      window.scrollTo(0,0);
+    }
+  }
+}
