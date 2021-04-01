08a6e7b808d64bb75b27882c06bfbee93a363751
Devin Harris <devinharris.316@gmail.com>
2021-04-01 18:45:31 -0400

    removed commented out code on profile bio

--- frontend/src/components/profile-bio/index.vue
+++ frontend/src/components/profile-bio/index.vue
@@ -1,37 +1,6 @@
 <!-- (c) Waveybits Inc. <2021> -->
 <!-- ALL RIGHTS RESERVED -->
 <template>
-  <!-- <div class="profile-bio">
-    <div class="user-profile">
-      <img v-if="user && user.profile_image" :src="getImage(user.profile_image)" />
-      <h4>{{ user.username }}</h4>
-      <router-link :to="{ name: 'Dev', params: { id: user.id }}" class="btn">Learn More</router-link>
-    </div>
-    <div class="user-info">
-      <div class="user-info_accounttype">
-        <h3>Roles:</h3>
-        <h4>{{ user.roles.join(", ") }}</h4>
-      </div>
-      <div class="user-info_hiringoptions">
-        <h3>Hiring Options:</h3>
-        <h4>{{ user.hiring_options.join(", ") }}</h4>
-      </div>
-      <div class="user-info_rating">
-        <h3>Rating:</h3>
-        <ul>
-          <li v-for="rate in user.rating" :key="rate"><font-awesome-icon style="color: #F0DB4F; font-size: 18px;" :icon="['fas', 'star']" /></li>
-        </ul>
-      </div>
-      <div class="user-info_skills">
-        <h3>Categories:</h3>
-        <h4>{{ skillsFormatted }}</h4>
-      </div>
-      <div class="user-bio">
-        <h3>Bio</h3>
-        <p>{{ user.bio }}</p>
-      </div>
-    </div>
-  </div> -->
   <div class="profile-bio">
 
     <div class="user-display">
