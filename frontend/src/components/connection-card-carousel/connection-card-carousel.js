1b1f6933498ab1d588f5d454d108f9ad315b57d4
roseman <jroseman@cincomcpq.com>
2021-04-22 21:56:13 -0400

    repush

---
+++ frontend/src/components/connection-card-carousel/connection-card-carousel.js
@@ -0,0 +1,73 @@
+// (c) Waveybits Inc. <2021>
+// ALL RIGHTS RESERVED
+import ConnectionCard from '@/components/connection-card';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { mapActions, mapMutations } from 'vuex'
+
+export default {
+  name: "ConnectionCardCarousel",
+  props: {
+    connections: {
+      type: Array,
+      required: true
+    },
+    numberOfCardsDisplayed: {
+      type: Number,
+      default: 5
+    }
+  },
+  data() {
+    return {
+      startIdx: 0,
+      endIdx: this.numberOfCardsDisplayed
+    }
+  },
+  components: {
+    ConnectionCard,
+    FontAwesomeIcon
+  },
+  computed: {
+    connectionsShown() {
+      return this.connections ? this.connections.slice(this.startIdx, this.endIdx) : [];
+    }
+  },
+  methods: {
+    ...mapActions(['fetchUserById', 'updateUser']),
+    ...mapMutations(['updateLoggedInUser']),
+    async removeConnection(connectionToRemove) {
+      let user = await this.getLoggedInUser
+      let id, response, updates
+      let newConnections = this.connections.map((connection) => connection.id).filter(connectionId => connectionId != connectionToRemove.id)
+      if (user) {
+        id = user.id
+        updates = {
+          connections: JSON.stringify(newConnections)
+        }
+        response = await this.updateUser({ id, updates })
+      }
+
+      if (response && user) {
+        this.toast.message = [
+          { text: 'You have successfully removed', emphasis: false },
+          { text: connectionToRemove.firstname + " " + connectionToRemove.lastname, emphasis: true },
+          { text: 'from your connections', emphasis: false }
+        ]
+        this.toast.type = 'success'
+        this.connections = this.connections.filter(connection => connection.id != connectionToRemove.id)
+        user.connections = newConnections
+        this.updateLoggedInUser(user)
+      } else {
+        this.toast.message = [
+          { text: 'You have', emphasis: false },
+          { text: 'unsuccessfully', emphasis: true },
+          { text: 'removed', emphasis: false },
+          { text: connectionToRemove.firstname + " " + connectionToRemove.lastname, emphasis: true },
+          { text: 'from your connections', emphasis: false }
+        ]
+        this.toast.type = 'error'
+      }
+      this.toast.duration = 5000
+      this.toast.isShown = true
+    }
+  }
+}
---
+++ frontend/src/components/connection-card-carousel/index.vue
@@ -0,0 +1,14 @@
+<!-- (c) Waveybits Inc. <2021> -->
+<!-- ALL RIGHTS RESERVED -->
+<template>
+  <div class="connection-card-carousel">
+     <font-awesome-icon :icon="['fas','arrow-alt-circle-left']" class="arrow-icon" />
+     <div class="connections">
+      <connection-card v-for="connection in connectionsShown" :connection="connection" :key="connection.id" @delete-click="removeConnection(connection)" />
+    </div>
+    <font-awesome-icon :icon="['fas','arrow-alt-circle-right']" class="arrow-icon" />
+  </div>
+</template>
+
+<script src="./connection-card-carousel.js"></script>
+<style src="./connection-card-carousel.scss" lang="scss" scoped></style>
