4c89cb4de9321d69fced90911a2fc9c671f585b2
roseman <jroseman@cincomcpq.com>
2021-04-30 21:33:12 -0400

    update to routes to accomodate db table changes

--- backend/db/queries/user-queries.js
+++ backend/db/queries/user-queries.js
@@ -10,18 +10,18 @@ module.exports = {
     if (param.username) {
       query.where('username', 'like', `%${param.username}%`)
       .leftJoin('developers', 'users.id', 'developers.user_id')
-      .select(['users.id','users.username','users.email','users.password', 'users.firstname','users.lastname', 'users.bio', 'users.visionary_categories','users.rating','users.profile_image', 'users.user_type_id', 'users.connections', 'users.notifications', 'users.tasks', 'users.notification_settings','users.subscription_settings','users.token_version',  'developers.dev_roles', 'developers.dev_categories', 'developers.dev_skills', 'developers.hiring_options', 'developers.portfolio', 'developers.dev_bio', 'developers.dev_rating'])
+      .select(['users.id','users.username','users.email','users.password', 'users.firstname','users.lastname', 'users.bio', 'users.visionary_categories','users.rating','users.profile_image', 'users.user_type_id', 'users.connections', 'users.notifications', 'users.tasks', 'users.notification_settings','users.subscription_settings','users.token_version',  'developers.dev_roles', 'developers.dev_categories', 'developers.dev_skills', 'developers.hiring_options', 'developers.portfolio', 'developers.dev_bio'])
       .first();
     }
     if (param.rating) {
       query.where('rating', param.rating)
       .leftJoin('developers', 'users.id', 'developers.user_id')
-      .select(['users.id','users.username','users.email','users.password', 'users.firstname','users.lastname', 'users.bio', 'users.visionary_categories','users.rating','users.profile_image', 'users.user_type_id', 'users.connections', 'users.notifications', 'users.tasks', 'users.notification_settings','users.subscription_settings','users.token_version',  'developers.dev_roles', 'developers.dev_categories', 'developers.dev_skills', 'developers.hiring_options', 'developers.portfolio', 'developers.dev_bio', 'developers.dev_rating'])
+      .select(['users.id','users.username','users.email','users.password', 'users.firstname','users.lastname', 'users.bio', 'users.visionary_categories','users.rating','users.profile_image', 'users.user_type_id', 'users.connections', 'users.notifications', 'users.tasks', 'users.notification_settings','users.subscription_settings','users.token_version',  'developers.dev_roles', 'developers.dev_categories', 'developers.dev_skills', 'developers.hiring_options', 'developers.portfolio', 'developers.dev_bio'])
     }
     if (param.email) {
       query.where('email', param.email)
       .leftJoin('developers', 'users.id', 'developers.user_id')
-      .select(['users.id','users.username','users.email','users.password', 'users.firstname','users.lastname', 'users.bio', 'users.visionary_categories','users.rating','users.profile_image', 'users.user_type_id', 'users.connections', 'users.notifications', 'users.tasks', 'users.notification_settings','users.subscription_settings','users.token_version',  'developers.dev_roles', 'developers.dev_categories', 'developers.dev_skills', 'developers.hiring_options', 'developers.portfolio', 'developers.dev_bio', 'developers.dev_rating'])
+      .select(['users.id','users.username','users.email','users.password', 'users.firstname','users.lastname', 'users.bio', 'users.visionary_categories','users.rating','users.profile_image', 'users.user_type_id', 'users.connections', 'users.notifications', 'users.tasks', 'users.notification_settings','users.subscription_settings','users.token_version',  'developers.dev_roles', 'developers.dev_categories', 'developers.dev_skills', 'developers.hiring_options', 'developers.portfolio', 'developers.dev_bio'])
       .first();
     }
     return query;
@@ -30,19 +30,19 @@ module.exports = {
     return knex('users')
           .where('users.id', id)
           .leftJoin('developers', 'users.id', 'developers.user_id')
-          .select(['users.id', 'users.username', 'users.email', 'users.password','users.firstname','users.lastname', 'users.bio', 'users.visionary_categories','users.rating','users.profile_image', 'users.user_type_id', 'users.connections', 'users.notifications', 'users.tasks', 'users.notification_settings','users.subscription_settings','users.token_version',  'developers.dev_roles', 'developers.dev_categories', 'developers.dev_skills', 'developers.hiring_options', 'developers.portfolio', 'developers.dev_bio', 'developers.dev_rating'])
+          .select(['users.id', 'users.username', 'users.email', 'users.password','users.firstname','users.lastname', 'users.bio', 'users.visionary_categories','users.ratings','users.profile_image', 'users.user_type_id', 'users.connections', 'users.notifications', 'users.notification_settings','users.subscription_settings','users.token_version',  'developers.dev_roles', 'developers.dev_categories', 'developers.dev_skills', 'developers.hiring_options', 'developers.portfolio', 'developers.dev_bio'])
           .first();
   },
   getDevUsers() {
     return knex('users')
             .where('user_type_id', 1)
             .join('developers', 'users.id', '=', 'developers.user_id')
-            .select(['users.id','users.username','users.email','users.password','users.firstname','users.lastname', 'users.bio', 'users.visionary_categories','users.rating','users.profile_image', 'users.user_type_id', 'users.connections', 'users.notifications', 'users.tasks', 'users.notification_settings','users.subscription_settings','users.token_version', 'developers.dev_roles', 'developers.dev_categories', 'developers.dev_skills', 'developers.hiring_options', 'developers.portfolio', 'developers.dev_bio', 'developers.dev_rating'])
+            .select(['users.id','users.username','users.email','users.password','users.firstname','users.lastname', 'users.bio', 'users.visionary_categories','users.ratings','users.profile_image', 'users.user_type_id', 'users.connections', 'users.notifications',  'users.notification_settings','users.subscription_settings','users.token_version', 'developers.dev_roles', 'developers.dev_categories', 'developers.dev_skills', 'developers.hiring_options', 'developers.portfolio', 'developers.dev_bio'])
   },
   getVisionaryUsers() {
     return knex('users')
             .where('user_type_id', 2)
-            .select(['users.id','users.username','users.email','users.password','users.firstname','users.lastname', 'users.bio', 'users.visionary_categories','users.rating','users.profile_image', 'users.user_type_id', 'users.connections', 'users.notifications', 'users.tasks', 'users.notification_settings','users.subscription_settings','users.token_version'])
+            .select(['users.id','users.username','users.email','users.password','users.firstname','users.lastname', 'users.bio', 'users.visionary_categories','users.ratings','users.profile_image', 'users.user_type_id', 'users.connections', 'users.notifications',  'users.notification_settings','users.subscription_settings','users.token_version'])
   },
   createDevUser({newUserInfo, newDevUserInfo}) {
     return knex("users")
