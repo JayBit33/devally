6c677987456c45a7c9585d221d7330b4e7dc1deb
Jay Roseman <jaybit33@gmail.com>
2021-05-10 23:13:16 -0400

    research data

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -5,10 +5,12 @@
     <div class="home-banner">
       <div class="col-1">
         <h1>Build A Team And</h1>
-        <h1> Make Your Tech Idea A Reality</h1>
+        <h1>Make Your Tech Idea A Reality</h1>
         <h4>Community of developers looking to join a team</h4>
         <h4>and build the next best thing since Jarvis.</h4>
-        <button class="btn"><router-link to="/devs" class="link">Get Started</router-link></button>
+        <button class="btn">
+          <router-link to="/devs" class="link">Get Started</router-link>
+        </button>
       </div>
       <div class="col-2">
         <img src="images/3593965.jpg" />
@@ -21,67 +23,94 @@
         <div>
           <img src="images/4071498.jpg" style="width: 300px" />
           <h2>Create A Project</h2>
-          <p>The path to making your idea a reality starts here! In 5 minutes or less you can create an account, a project and start searching for the people you need to bring it to life.
-          Don't let someone beat you to the finish line. Get started on your project today!</p>
-          <router-link to="/devs" class="link"><button>Create Project</button></router-link>
+          <p>
+            The path to making your idea a reality starts here! In 5 minutes or
+            less you can create an account, a project and start searching for
+            the people you need to bring it to life. Don't let someone beat you
+            to the finish line. Get started on your project today!
+          </p>
+          <router-link to="/devs" class="link"
+            ><button>Create Project</button></router-link
+          >
         </div>
         <div>
           <img src="images/team.png" style="width: 300px" />
           <h2>Build A Team</h2>
-          <p>Great teams build great products. Search our community to find the professionals you need. Whether you are in need of a graphic designer, web developer, mobile app dev, project manager, dev-ops or all of the above, you can find them all here. </p>
-          <router-link to="/devs" class="link"><button>Search Devs</button></router-link>
+          <p>
+            Great teams build great products. Search our community to find the
+            professionals you need. Whether you are in need of a graphic
+            designer, web developer, mobile app dev, project manager, dev-ops or
+            all of the above, you can find them all here.
+          </p>
+          <router-link to="/devs" class="link"
+            ><button>Search Devs</button></router-link
+          >
         </div>
         <div>
           <img src="images/4309857.jpg" style="width: 300px" />
           <h2>Bring Your Idea To Life</h2>
-          <p>As you and your new team begin the journey of designing and creating your product we will still be here for you. We know it is a lot to accomplish and you will have questions along the way.
-           We have articles that help to guide you on your journey.</p>
-          <router-link to="/devs" class="link"><button>View Articles</button></router-link>
+          <p>
+            As you and your new team begin the journey of designing and creating
+            your product we will still be here for you. We know it is a lot to
+            accomplish and you will have questions along the way. We have
+            articles that help to guide you on your journey.
+          </p>
+          <router-link to="/devs" class="link"
+            ><button>View Articles</button></router-link
+          >
         </div>
       </div>
     </div>
 
     <div class="join-devs">
       <div>
-        <img src="images/programmer-man.png" style="width:500px" />
+        <img src="images/programmer-man.png" style="width: 500px" />
       </div>
       <div class="join-devs_col2">
         <h2>Want To Build Something From the Ground Up?</h2>
-        <p>Are you a web, mobile or software developer, graphic designer or project manager? Join DevAlly and find paid jobs or work for part ownership in tech startups.</p>
-        <router-link to="/signin" class="link"><button>Join Now</button></router-link>
+        <p>
+          Are you a web, mobile or software developer, graphic designer or
+          project manager? Join DevAlly and find paid jobs or work for part
+          ownership in tech startups.
+        </p>
+        <router-link to="/signin" class="link"
+          ><button>Join Now</button></router-link
+        >
       </div>
     </div>
 
     <div class="start-learning">
       <div>
         <h2>Need Guidance on how to start and run a tech startup?</h2>
-        <p>We got you covered. Read up on software development methodologies, how to start a business and much more.</p>
-        <router-link to="/signin" class="link"><button>Start Learning</button></router-link>
+        <p>
+          We got you covered. Read up on software development methodologies, how
+          to start a business and much more.
+        </p>
+        <router-link to="/signin" class="link"
+          ><button>Start Learning</button></router-link
+        >
       </div>
       <div>
-        <img :src="getImage('201brain-knowledge.png')" style="width:350px" />
+        <img :src="getImage('201brain-knowledge.png')" style="width: 350px" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "Home",
   props: [],
-  created() {
-  },
+  created() {},
   methods: {
     getImage(imageName) {
-      return require(`@/assets/${imageName}`)
-    }
-  }
-}
+      return require(`@/assets/${imageName}`);
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
 .main {
   // max-width: 1780px;
   background-color: white;
@@ -95,22 +124,22 @@ export default {
     margin-bottom: 3rem;
     margin-left: 9rem;
     margin-right: 7rem;
-    border-bottom: 1px solid #F2F2F2;
+    border-bottom: 1px solid #f2f2f2;
     background-color: white;
     .col-1 {
       margin-left: 2.5rem;
       h1 {
-        font: 700 2.125rem 'Montserrat';
+        font: 700 2.125rem "Montserrat";
         color: $primary;
         line-height: 1.5rem;
       }
       h4 {
-        font: 1.125rem 'Montserrat';
-        line-height: .5rem;
+        font: 1.125rem "Montserrat";
+        line-height: 0.5rem;
       }
       button {
         background-color: $button-primary;
-        padding: 1rem 4rem .75rem 4rem;
+        padding: 1rem 4rem 0.75rem 4rem;
         font-size: 1.25rem;
         border: none;
         float: left;
@@ -131,13 +160,15 @@ export default {
   .getting-started {
     margin: 5rem 0;
     h1 {
-      font-size: 2.5rem; color: #514E4E;
+      font-size: 2.5rem;
+      color: #514e4e;
       text-decoration: underline;
       text-align: center;
       margin-bottom: 7rem;
     }
     h2 {
-      font-size: 2.5rem; color: #514E4E;
+      font-size: 2.5rem;
+      color: #514e4e;
       text-decoration: underline;
       text-align: center;
     }
@@ -156,7 +187,7 @@ export default {
         align-items: center;
       }
       h2 {
-        font-family: 'Montserrat';
+        font-family: "Montserrat";
         font-size: 2rem;
         text-decoration: none;
         color: black;
@@ -164,19 +195,21 @@ export default {
       }
       p {
         width: 30rem;
-        font-family: 'Segoe UI';
+        font-family: "Segoe UI";
         font-size: 1.25rem;
         line-height: 2rem;
         text-align: center;
       }
-      img { width: 400px; }
+      img {
+        width: 400px;
+      }
       .link {
-          color: white;
+        color: white;
         button {
           display: block;
           background-color: $button-secondary;
           color: white;
-          font: 1.25rem 'Montserrat';
+          font: 1.25rem "Montserrat";
           text-transform: uppercase;
           margin-top: 2rem;
           padding: 1rem 2rem;
@@ -202,13 +235,13 @@ export default {
       margin-right: 9rem;
     }
     h2 {
-      font-family:'Montserrat';
+      font-family: "Montserrat";
       font-size: 2rem;
       text-align: center;
       color: black;
     }
     p {
-      font-family: 'Segoe UI';
+      font-family: "Segoe UI";
       font-size: 1.25rem;
       line-height: 2rem;
       text-align: center;
@@ -220,7 +253,7 @@ export default {
         display: block;
         background-color: $button-secondary;
         color: white;
-        font: 1.25rem 'Montserrat';
+        font: 1.25rem "Montserrat";
         text-transform: uppercase;
         margin-top: 2rem;
         padding: 1rem 2rem;
@@ -247,7 +280,7 @@ export default {
       align-items: center;
     }
     h2 {
-      font-family:'Montserrat';
+      font-family: "Montserrat";
       font-size: 2rem;
       line-height: 3rem;
       text-align: left;
@@ -255,7 +288,7 @@ export default {
       color: black;
     }
     p {
-      font-family: 'Segoe UI';
+      font-family: "Segoe UI";
       font-size: 1.25rem;
       line-height: 2rem;
       text-align: left;
@@ -267,7 +300,7 @@ export default {
         display: block;
         background-color: $button-secondary;
         color: white;
-        font: 1.25rem 'Montserrat';
+        font: 1.25rem "Montserrat";
         text-transform: uppercase;
         margin-top: 2rem;
         padding: 1rem 2rem;
@@ -283,9 +316,9 @@ export default {
   // remove after inserting <router-link> for all buttons
   button {
     display: block;
-    background-color: #6B8BA4;
+    background-color: #6b8ba4;
     color: white;
-    font: 1.25rem 'Montserrat';
+    font: 1.25rem "Montserrat";
     text-transform: uppercase;
     margin-top: 2rem;
     padding: 1rem 2rem;
@@ -295,9 +328,8 @@ export default {
   }
 }
 
-
 // current header issues below 1652
-@media (min-width: 1652px)  {
+@media (min-width: 1652px) {
   .main {
     justify-content: center;
     margin: 0 auto;
