408991ecac4bcfcb0d58dbf8134860e70d36c80a
Devin Harris <devinharris.316@gmail.com>
2021-05-12 14:48:19 -0400

    style and connection card click on edit a new project bug fix

--- frontend/src/components/connection-card/index.vue
+++ frontend/src/components/connection-card/index.vue
@@ -3,7 +3,7 @@
 <template>
   <div class="connection-card" :class="{'minimized': isMinimized, 'isChecked': isChecked}" @click="$emit('check-click')">
     <font-awesome-icon v-if="!isMinimized && hasDeleteIcon && getLoggedInUser.id != connection.id" @click="$emit('delete-click')" :icon="['fas','trash']" class="delete-icon"></font-awesome-icon>
-    <font-awesome-icon v-if="isMinimized" @click="$emit('check-click')" :icon="isChecked ? ['fas','check-square'] : ['far','square']" class="check-icon"></font-awesome-icon>
+    <font-awesome-icon v-if="isMinimized" @click.stop="$emit('check-click')" :icon="isChecked ? ['fas','check-square'] : ['far','square']" class="check-icon"></font-awesome-icon>
     <div v-if="isMinimized" class="connection-card-overlay" :class="{'hidden': isChecked}"></div>
     <img class="connection-card-avatar" :src="getImage(connection.profile_image)" />
     <h4 class="connection-card-fullname">{{ fullName }}</h4>
