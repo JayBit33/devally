f7b7f64125e2e5a116abf5175a917bc00d98cb1a
Justin Roseman <jaybit33@gmail.com>
2021-06-04 23:28:31 -0400

    added tabs to switch between visionary and developer homepage copywrite

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -17,81 +17,104 @@
       </div>
     </div>
 
-    <div class="getting-started">
-      <h1>Getting Started Is Simple</h1>
-      <div class="getting-started_steps">
+    <div class="choose-account-type">
+      <button :class="{ 'active' : visonaryCopywrite }" @click="toggleContent('visionary')">Visionary</button>
+      <button :class="{ 'active' : developerCopywrite }" @click="toggleContent('developer')">Developer</button>
+    </div>
+
+    <div v-if="visonaryCopywrite">
+      <div class="getting-started">
+        <h1>Getting Started Is Simple</h1>
+        <div class="getting-started_steps">
+          <div>
+            <img src="images/4071498.jpg" style="width: 300px" />
+            <h2>Create A Project</h2>
+            <p>
+              The path to making your idea a reality starts here! In 5 minutes or
+              less you can create an account, a project and start searching for
+              the people you need to bring it to life. Don't let someone beat you
+              to the finish line. Get started on your project today!
+            </p>
+            <router-link to="/devs" class="link"
+              ><button>Create Project</button></router-link
+            >
+          </div>
+          <div>
+            <img src="images/team.png" style="width: 300px" />
+            <h2>Build A Team</h2>
+            <p>
+              Great teams build great products. Search our community to find the
+              professionals you need. Whether you are in need of a graphic
+              designer, web developer, mobile app dev, project manager, dev-ops or
+              all of the above, you can find them all here.
+            </p>
+            <router-link to="/devs" class="link"
+              ><button>Search Devs</button></router-link
+            >
+          </div>
+          <div>
+            <img src="images/4309857.jpg" style="width: 300px" />
+            <h2>Bring Your Idea To Life</h2>
+            <p>
+              As you and your new team begin the journey of designing and creating
+              your product we will still be here for you. We know it is a lot to
+              accomplish and you will have questions along the way. We have
+              articles that help to guide you on your journey.
+            </p>
+            <router-link to="/devs" class="link"
+              ><button>View Articles</button></router-link
+            >
+          </div>
+        </div>
+      </div>
+      <div class="join-devs">
         <div>
-          <img src="images/4071498.jpg" style="width: 300px" />
-          <h2>Create A Project</h2>
+          <img src="images/programmer-man.png" style="width: 500px" />
+        </div>
+        <div class="join-devs_col2">
+          <h2>Want To Build Something From the Ground Up?</h2>
           <p>
-            The path to making your idea a reality starts here! In 5 minutes or
-            less you can create an account, a project and start searching for
-            the people you need to bring it to life. Don't let someone beat you
-            to the finish line. Get started on your project today!
+            Are you a web, mobile or software developer, graphic designer or
+            project manager? Join DevAlly and find paid jobs or work for part
+            ownership in tech startups.
           </p>
-          <router-link to="/devs" class="link"
-            ><button>Create Project</button></router-link
+          <router-link to="/signin" class="link"
+            ><button>Join Now</button></router-link
+          >
+        </div>
+      </div>
+      <div class="start-learning">
+        <div>
+          <h2>Need Guidance on how to start and run a tech startup?</h2>
+          <p>
+            We got you covered. Read up on software development methodologies, how
+            to start a business and much more.
+          </p>
+          <router-link to="/signin" class="link"
+            ><button>Start Learning</button></router-link
           >
         </div>
         <div>
-          <img src="images/team.png" style="width: 300px" />
-          <h2>Build A Team</h2>
-          <p>
-            Great teams build great products. Search our community to find the
-            professionals you need. Whether you are in need of a graphic
-            designer, web developer, mobile app dev, project manager, dev-ops or
-            all of the above, you can find them all here.
-          </p>
-          <router-link to="/devs" class="link"
-            ><button>Search Devs</button></router-link
-          >
-        </div>
-        <div>
-          <img src="images/4309857.jpg" style="width: 300px" />
-          <h2>Bring Your Idea To Life</h2>
-          <p>
-            As you and your new team begin the journey of designing and creating
-            your product we will still be here for you. We know it is a lot to
-            accomplish and you will have questions along the way. We have
-            articles that help to guide you on your journey.
-          </p>
-          <router-link to="/devs" class="link"
-            ><button>View Articles</button></router-link
-          >
+          <img :src="getImage('201brain-knowledge.png')" style="width: 350px" />
         </div>
       </div>
     </div>
-
-    <div class="join-devs">
-      <div>
-        <img src="images/programmer-man.png" style="width: 500px" />
-      </div>
-      <div class="join-devs_col2">
-        <h2>Want To Build Something From the Ground Up?</h2>
-        <p>
-          Are you a web, mobile or software developer, graphic designer or
-          project manager? Join DevAlly and find paid jobs or work for part
-          ownership in tech startups.
-        </p>
-        <router-link to="/signin" class="link"
-          ><button>Join Now</button></router-link
-        >
-      </div>
-    </div>
-
-    <div class="start-learning">
-      <div>
-        <h2>Need Guidance on how to start and run a tech startup?</h2>
-        <p>
-          We got you covered. Read up on software development methodologies, how
-          to start a business and much more.
-        </p>
-        <router-link to="/signin" class="link"
-          ><button>Start Learning</button></router-link
-        >
-      </div>
-      <div>
-        <img :src="getImage('201brain-knowledge.png')" style="width: 350px" />
+    <div v-else >
+      <div class="join-devs">
+        <div>
+          <img src="images/programmer-man.png" style="width: 500px" />
+        </div>
+        <div class="join-devs_col2">
+          <h2>Want To Build Something From the Ground Up?</h2>
+          <p>
+            Are you a web, mobile or software developer, graphic designer or
+            project manager? Join DevAlly and find paid jobs or work for part
+            ownership in tech startups.
+          </p>
+          <router-link to="/signin" class="link"
+            ><button>Join Now</button></router-link
+          >
+        </div>
       </div>
     </div>
   </div>
@@ -101,11 +124,27 @@
 export default {
   name: "Home",
   props: [],
+  data() {
+    return {
+      visonaryCopywrite: true,
+      developerCopywrite: false
+
+    }
+  },
   created() {},
   methods: {
     getImage(imageName) {
       return require(`@/assets/${imageName}`);
     },
+    toggleContent(view) {
+      if (view === 'visionary') {
+        this.visonaryCopywrite = true
+        this.developerCopywrite = false 
+      } else {
+        this.developerCopywrite = true 
+        this.visonaryCopywrite = false
+      }
+    }
   },
 };
 </script>
@@ -121,7 +160,7 @@ export default {
     grid-template-columns: 3fr auto;
     column-gap: 12rem;
     align-items: center;
-    margin-bottom: 3rem;
+    margin-bottom: 0rem;
     margin-left: 9rem;
     margin-right: 7rem;
     border-bottom: 1px solid #f2f2f2;
@@ -147,6 +186,10 @@ export default {
         .link {
           color: white;
         }
+
+        &:hover {
+          background-color: darken($button-primary, 10%);
+        }
       }
     }
     .col-2 {
@@ -155,6 +198,35 @@ export default {
         width: 40rem;
       }
     }
+  }
+
+  .choose-account-type {
+    display: flex;
+    // max-width: 22rem;
+    // margin: 0 auto;
+
+    button {
+      background-color: white;
+      color: black;
+      border: 1px solid lightgrey;
+      margin-top: .25rem;
+      width: 50%;
+
+      &:hover {
+        background-color: $button-tri;
+        color: white;
+      }
+    }
+
+    button:nth-of-type(2) {
+      border-width: 1px 1px 1px 0;
+    }
+
+    .active {
+      background-color: $button-tri;
+      color: white;
+    }
+
   }
 
   .getting-started {
@@ -217,6 +289,10 @@ export default {
           border-radius: 1rem;
           margin: 2rem auto;
           cursor: pointer;
+
+          &:hover {
+            background-color: lighten($button-secondary, 20%);
+          }
         }
       }
     }
@@ -261,6 +337,10 @@ export default {
         border-radius: 2rem;
         margin: 2rem auto;
         cursor: pointer;
+
+        &:hover {
+          background-color: lighten($button-secondary, 20%);
+        }
       }
     }
   }
@@ -309,6 +389,10 @@ export default {
         margin: 1rem auto;
         float: left;
         cursor: pointer;
+
+        &:hover {
+          background-color: lighten($button-secondary, 20%);
+        }
       }
     }
   }
